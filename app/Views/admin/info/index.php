8935fad049d0bfba0c6708955fd8d161f2620950
arimahendra1806 <arimahendra1806@gmail.com>
2023-03-04 15:23:04 +0700

    [update] : ganti icon

--- app/Views/admin/info/index.php
+++ app/Views/admin/info/index.php
@@ -9,8 +9,8 @@
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Kategori</th>
-                        <th>Detail Pesan</th>
+                        <th data-orderable="false">Kategori</th>
+                        <th>Detail Informasi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -19,14 +19,14 @@
                         <?php if (date('Y-m-d', strtotime($row['created_at'])) === date('Y-m-d')) : ?>
                             <tr class="bg-warning">
                                 <td class="text-primary"><?= $no++; ?></td>
-                                <td class="text-primary text-center"><?= ($row['kategori'] == 'insert') ? '<i data-feather="plus-circle">' : (($row['kategori'] == 'update') ? '<i data-feather="play-circle">' : '<i data-feather="minus-circle">'); ?></i></td>
+                                <td class="text-primary text-center"><?= ($row['kategori'] == 'insert') ? '<i data-feather="plus-circle">' : (($row['kategori'] == 'update') ? '<i data-feather="info">' : '<i data-feather="minus-circle">'); ?></i></td>
                                 <td class="text-primary"><?= $row['detail_pesan']; ?></td>
                             </tr>
                         <?php endif; ?>
                         <?php if (date('Y-m-d', strtotime($row['created_at'])) !== date('Y-m-d')) : ?>
                             <tr>
                                 <td><?= $no++; ?></td>
-                                <td class="text-primary text-center"><?= ($row['kategori'] == 'insert') ? '<i data-feather="plus-circle">' : (($row['kategori'] == 'update') ? '<i data-feather="play-circle">' : '<i data-feather="minus-circle">'); ?></i></td>
+                                <td class="text-primary text-center"><?= ($row['kategori'] == 'insert') ? '<i data-feather="plus-circle">' : (($row['kategori'] == 'update') ? '<i data-feather="info">' : '<i data-feather="minus-circle">'); ?></i></td>
                                 <td><?= $row['detail_pesan']; ?></td>
                             </tr>
                         <?php endif; ?>
